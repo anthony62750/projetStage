103a7b233e768a5d210d2eb9f473791892e170f1
Yacine-J <yacinej@outlook.fr>
2023-11-23 16:00:29 +0100

    import react

--- src/pages/Inscription.jsx
+++ src/pages/Inscription.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const Inscription = () => {
   return (
     <div>
--- src/pages/Planning.jsx
+++ src/pages/Planning.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const Planning = () => {
   return (
     <div>
