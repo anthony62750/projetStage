403b72c2547345071ef31a171f4445d63910087c
Yacine-J <yacinej@outlook.fr>
2023-12-13 15:32:58 +0100

    modalité

--- src/components/page_inscription/Modalites.jsx
+++ src/components/page_inscription/Modalites.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import image from '../page_inscription/image_dossiers.jpg';
 
 export const Modalites = () => {
     return (
-        <section className='flex justify-between'>
-            <div className='lg:ml-6 lg:mt-44 drop-shadow-2xl'>
-                <img className='lg:w-68' src={image} alt='img'></img>
-            </div>
-            
-        </section>
+        <div>
+            <section className='flex justify-between lg:mt-44 lg:ml-6'>
+                <div>
+                    <h1 className='lg:text-4xl'>Modalités d'inscription: </h1><br></br>
+                    <p className='lg:text-xl'>L'accès à nos installations est ouvert aux mineurs à partir de 16 ans, sous certaines conditions. Les mineurs de 16 ans doivent être accompagnés d'un tuteur déjà inscrit à la salle. Pour les mineurs âgés de 16 ans et demi, une autorisation parentale est requise en plus de la présence d'un parent au moment de l'inscription. Nous accordons une importance particulière à la sécurité et au bien-être de nos membres.<br></br>
+
+                    Nous insistons sur l'importance d'apporter son propre matériel lors de chaque séances, notamment une serviette et une double paire de chaussures, cette dernière étant obligatoire pour garantir des conditions sanitaires optimales.<br></br>
+
+                    Lors de l'inscription, il est impératif de fournir le règlement signé avec la mention "lu et approuvé". De plus, la souscription à une assurance responsabilité civile est OBLIGATOIRE. Les mineurs et les majeurs doivent également remplir un questionnaire de santé.<br></br>
+
+                    Nous soulignons qu'aucune inscription ne sera acceptée si le dossier est incomplet. Ces mesures visent à assurer la sécurité, le confort et le respect des normes éthiques au sein de notre établissement. Nous sommes reconnaissants de votre compréhension et de votre coopération pour maintenir un environnement sain et accueillant pour tous nos membres.</p>
+                </div>
+            </section>
+        </div>
     );
 }
