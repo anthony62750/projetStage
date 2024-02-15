4f76b1e4a3f5f4445578148df31aee31489cc683
allard.anthony@hotmail.fr <allard.anthony@hotmail.fr>
2024-02-15 13:29:01 +0100

    button fait

--- src/components/page_inscription/Modalites.jsx
+++ src/components/page_inscription/Modalites.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+
 
 export const Modalites = () => {
+
+    const [index, setIndex] = useState(null);
+
+    useEffect(() => {
+
+        const mineurs = document.querySelectorAll(".mineur")
+        
+        mineurs.forEach((mineur, i) => {
+            if (index == i) {
+                mineur?.classList.add('active')
+            } else {
+                mineur?.classList.remove('active')
+            }
+        })
+    }
+    , [index])
     
+
     return (
         <div>
             <section className='min-h-screen p-20 flex justify-between xs:text-xs xs:mt-32 xs:ml-1 sm:text-sm sm:mt-32 sm:ml-2 md:text-base md:mt-36 md:ml-4 lg:text-xl lg:ml-6'>
@@ -22,17 +41,31 @@ export const Modalites = () => {
                     - Une assurance responsabilité civile,<br></br>
                     
                     <p> - Une attestation a faire copier aux parents des mineurs de 16 ans et demi : </p>
-                    <a className='text-[#0a62d0]' href={process.env.PUBLIC_URL + '/Autorisation_parentale.pdf'} target="_blank" rel="noopener noreferrer">Ouvrir le pdf</a><br></br>
+                    <button class=" justify-center items-center rounded pr-3 pl-3 mt-3 mb-3 mr-3 h-10 w-38 border text-[#000000] bg-[#0a62d0]" type='button' onClick={() =>setIndex(index == 0 ? null : 0)}>Afficher le pdf</button>
+                    
+                    <div className="mineur" >
+                        <a className='text-[#0a62d0]' href={process.env.PUBLIC_URL + '/Autorisation_parentale.pdf'} target="_blank" rel="noopener noreferrer">Ouvrir le pdf</a><br></br>
+                    </div>
 
                     <p> - Un questionnaire de santé pour les mineurs : </p>
-                    <a className='text-[#0a62d0]' href={process.env.PUBLIC_URL + '/Questionnaire_mineur.pdf'} target="_blank" rel="noopener noreferrer">Ouvrir le pdf</a><br></br>
+                    <button  class="justify-center items-center rounded pr-3 pl-3 mt-3 mb-3 mr-3 h-10 w-38 border text-[#000000] bg-[#0a62d0]" type='button' onClick={() =>setIndex(index == 1 ? null : 1)}>Afficher le pdf</button>
+                    <div className="mineur" >
+
+                        <a className='text-[#0a62d0]' href={process.env.PUBLIC_URL + '/Questionnaire_mineur.pdf'} target="_blank" rel="noopener noreferrer">Ouvrir le pdf</a><br></br>
+                    </div>
 
                     <p> - Un questionnaire de santé pour les majeurs : </p>
-                    <a className='text-[#0a62d0]' href={process.env.PUBLIC_URL + '/Questionnaire_majeur.pdf'} target="_blank" rel="noopener noreferrer">Ouvrir le pdf</a><br></br>
+                    <button class="justify-center items-center rounded pr-3 pl-3 mt-3 mb-3 mr-3 h-10 w-38 border text-[#000000] bg-[#0a62d0]" type='button' onClick={() =>setIndex(index == 2 ? null : 2)}>afficher le pdf</button>
+                    <div className='mineur'>
+                        <a className='text-[#0a62d0]' href={process.env.PUBLIC_URL + '/Questionnaire_majeur.pdf'} target="_blank" rel="noopener noreferrer">Ouvrir le pdf</a><br></br>
+                    </div>
 
                     <p> - Règlement signé avec la mention "lu et approuvé" : </p>
-                    <a className='text-[#0a62d0]' href={process.env.PUBLIC_URL + '/Reglement_interieur.pdf'} target="_blank" rel="noopener noreferrer">Ouvrir le pdf</a><br></br>
+                    <button class="justify-center items-center rounded pr-3 pl-3 mt-3 mb-3 mr-3 h-10 w-38 border text-[#000000] bg-[#0a62d0]" type='button' onClick={() =>setIndex(index == 3 ? null : 3)}>afficher le pdf</button>
+                    <div className="mineur">
 
+                        <a className='text-[#0a62d0]' href={process.env.PUBLIC_URL + '/Reglement_interieur.pdf'} target="_blank" rel="noopener noreferrer">Ouvrir le pdf</a><br></br>
+                    </div>
 
                     </p>
                 </div>
